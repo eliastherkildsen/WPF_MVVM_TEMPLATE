6f88a402aa446f3641a422ee33cbc78e5fb1cb2a
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-10-06 11:01:05 +0200

    fix(InterfaceAddapter layer): InterfaceAddapter
    added InterfaceAddapter folder. w. temp file to ensure that git does not remove the folder.

---
+++ InterfaceAdapter/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF_MVVM_TEMPLATE.InterfaceAdapter;
+
+public interface IFileService
+{
+    bool IsDirectoryAvailable(string directory);
+    bool DirectoryContainsFiles(string directory); 
+    List<string> GetFiles(string directory);
+    bool FileIsOfType(string filename, string type);
+}
