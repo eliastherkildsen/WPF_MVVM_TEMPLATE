150b1572da1d8020a19eb5fd335ef7a6537f23ac
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-10-06 11:02:22 +0200

    fix(Infrastructur layer): Infrastructur
    added Infrastructur folder w. temp file to ensure that git does not remove the folder on commit.

---
+++ Infrastructure/FileService.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+
+namespace WPF_MVVM_TEMPLATE.Infrastructure;
+
+public class FileService
+{
+    /// <summary>
+    /// Method for validating if a directory exists
+    /// </summary>
+    /// <param name="directory"> the absolute path to the directory </param>
+    /// <returns></returns>
+    public bool IsDirectoryAvailable(string directory)
+    {
+        return Directory.Exists(directory);
+    }
+    
+    
+    /// <summary>
+    /// Method for validating that a directory contains files. 
+    /// </summary>
+    /// <param name="directory"> the absolute path to the directory  </param>
+    /// <returns></returns>
+    /// <exception cref="DirectoryNotFoundException"> thrown if directory does not exist </exception>
+    public bool DirectoryContainsFiles(string directory)
+    {
+        if (IsDirectoryAvailable(directory))
+        {
+            return Directory.GetFiles(directory).Any();
+        }
+        
+        throw new DirectoryNotFoundException();
+        
+    }
+    
+    /// <summary>
+    /// Method for fetching the path of all files in a directory.
+    /// </summary>
+    /// <param name="directory"> the absolute path to the directory </param>
+    /// <returns></returns>
+    /// <exception cref="DirectoryNotFoundException"> thrown if directory does not exist </exception>
+    /// <exception cref="FileNotFoundException"> thrown if directory does not contain files </exception>
+    public List<string> GetFiles(string directory)
+    {
+        
+        if (!IsDirectoryAvailable(directory)) throw new DirectoryNotFoundException();
+        if (!DirectoryContainsFiles(directory)) throw new FileNotFoundException();
+        
+        return Directory.GetFiles(directory).ToList();
+        
+    }
+    
+    
+    /// <summary>
+    /// Method for comparing the file extensions of a file
+    /// </summary>
+    /// <param name="filename"> filename with extensions</param>
+    /// <param name="type"> expected extensions ex. "xml"</param>
+    /// <returns></returns>
+    public bool FileIsOfType(string filename, string type)
+    {
+        var splitFile = filename.Split(".");
+        return splitFile.Length != 0 && splitFile[splitFile.Length - 1].Equals(type);
+    }
+}
